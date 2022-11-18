0434bd4c14a839cded6205ce6d07dae5ed6efb96
tony <attila.kovacs92@gmail.com>
2022-11-18 15:43:27 +0100

    add tailwindUI and breadcrumbs component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Breadcrumbs from "../components/Breadcrumbs";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-red-500">Hello World</h1>
+      <Breadcrumbs />
     </div>
   );
 }
